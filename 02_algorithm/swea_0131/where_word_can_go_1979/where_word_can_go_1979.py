fb7f8fc0ee8c5ea6a15d9bdaf048d00f5315590b
jhpark0323 <brianwogus@naver.com>
2024-01-31 23:34:35 +0900

    swea_0131

--- 02_algorithm/swea_0131/where_word_can_go_1979/where_word_can_go_1979.py
+++ 02_algorithm/swea_0131/where_word_can_go_1979/where_word_can_go_1979.py
@@ -10,60 +10,58 @@ for test_case in range(1, T+1):
 
     n, k = map(int, input().split())
 
-    puzzle = [list(map(int, input().split())) for _ in range(n)]
+    # puzzle에 input을 받는데 행렬의 겉을 0으로 감쌈 -> index걱정 안해도 될듯
+    puzzle = [[0] * (n+2)]
+    for _ in range(n):
+        puzzle.append([0] + list(map(int, input().split())) + [0])
+    puzzle.append([0] * (n+2))
 
-    # pp.pprint(puzzle)
+    pp.pprint(puzzle)
+
     total = 0
-
-    # 세로기준
-    # 행의 갯수만큼 순회
-    for row in range(n-k+1):
-        # 열의 갯수만큼 순회
-        for col in range(n):
-            t_f_length = 0
-            # t_f에 1씩 더해서 k이면 k자리 이상의 글자가 들어갈 수 있음
-            for i in range(k):
-                if puzzle[row+i][col] == 1:
-                   t_f_length += 1
-            # 다 더했을 때 k이면 뒤에 또 1이있는지 검사, 이때 범위안에 들어가는지 체크
-            if (t_f_length == k) & (row+k == n):  # 다 더했을 때 k이고, 그 뒤는 벽일때
-                if row-1 == -1:  # 앞에도 벽일 때
-                  total += 1
-                elif row-1 >= 0:  # 앞에는 벽이 아닐때
-                    if puzzle[row-1][col] == 0:
-                        total += 1
-            elif (t_f_length == k) & (row+k < n):  # 다 더했을 때 k이고, 그 뒤가 벽이 아닐 때
-                if puzzle[row+k][col] == 0:  # 그 뒤가 0이고
-                    if row - 1 == -1:  # 앞에도 벽일 때
-                        total += 1
-                    elif row - 1 >= 0:  # 앞에는 벽이 아닐때
-                        if puzzle[row - 1][col] == 0:
-                            total += 1
-
-    # 가로기준
-    # 행의 갯수만큼 순회
-    for row in range(n):
-        # 열의 갯수만큼 순회
-        for col in range(n-k+1):
-            t_f_width = 0
-            # t_f에 1씩 더해서 k이면 k자리 이상의 글자가 들어갈 수 있음
-            for i in range(k):
-                if puzzle[row][col+i] == 1:
-                    t_f_width += 1
-            # 다 더했을 때 k이면 뒤에 또 1이있는지 검사, 이때 범위안에 들어가는지 체크
-            if (t_f_width == k) & (col + k == n):  # 다 더했을 때 k이고, 그 뒤는 벽일때
-                if col - 1 == -1:  # 앞에도 벽일 때
+    # 행을 하나의 문자열로 만들기
+    for row_ls in puzzle:
+        row = ''.join(map(str, row_ls))
+        # print(row)
+        word = '0' + '1' * k + '0'
+        # print(word)
+        if word in row:
+            # count함수 쓸라했는데 중복하는거 체크 못해서 일일이 셈
+            # row를 슬라이싱으로 잘라서 생기는 모든 경우를 word와 비교해서 같으면 total += 1
+            for cnt in range(len(row) - len(word) + 1):
+                if row[cnt:cnt + len(word)] == word:
                     total += 1
-                elif col - 1 >= 0:  # 앞에는 벽이 아닐때
-                    if puzzle[row - 1][col] == 0:
-                        total += 1
-            elif (t_f_width == k) & (col + k < n):  # 다 더했을 때 k이고, 그 뒤는 벽이 아닐때
-                if puzzle[row][col+k] == 0:  # 그 뒤가 0이고
-                    if col - 1 == -1:  # 앞에도 벽일 때
-                        total += 1
-                    elif col - 1 >= 0:  # 앞에는 벽이 아닐때
-                        if puzzle[row][col-1] == 0:
-                            total += 1
+    # print(total)
+
+    # 열을 하나의 문자열로 만들기위해 puzzle_transpose로 전치행렬로 만들어서 행으로 만든방법 그대로 쓸거임
+    puzzle_transpose = [[0]*(n+2) for _ in range(n+2)]
+
+    # 전치행렬 만들기
+    for i in range(n+2):
+        for j in range(n+2):
+            puzzle_transpose[i][j] = puzzle[j][i]
+    # pp.pprint(puzzle_transpose)
+
+    # 열을 하나의 문자열로 만들기 -> 전치행렬을 이용해 행으로 만들기
+    for row_ls in puzzle_transpose:
+        row = ''.join(map(str, row_ls))
+        # print(row)
+        word = '0' + '1' * k + '0'
+        # print(word)
+        if word in row:
+            # count함수 쓸라했는데 중복하는거 체크 못해서 일일이 셈
+            # row를 슬라이싱으로 잘라서 생기는 모든 경우를 word와 비교해서 같으면 total += 1
+            for cnt in range(len(row) - len(word) + 1):
+                if row[cnt:cnt + len(word)] == word:
+                    total += 1
+    print(f'#{test_case} {total}')
 
 
-    print(total)
+# for cnt in range(len(row) - len(word) + 1):
+#     if row[cnt:cnt+len(word)] == word:
+#         total += 1
+
+# word = '01110111011100' # 14
+# w = '01110'  # 5
+# print(word.count(w))
+# print(len(word), len(w))
