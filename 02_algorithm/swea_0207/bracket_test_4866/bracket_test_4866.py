5928ea772c41428333217a3968e5fc898b3600ab
jhpark0323 <brianwogus@naver.com>
2024-02-06 22:13:17 +0900

    swea_0207

---
+++ 02_algorithm/swea_0207/bracket_test_4866/bracket_test_4866.py
@@ -0,0 +1,40 @@
+import sys
+sys.stdin = open('sample_input.txt')
+
+T = int(input())
+
+for test_case in range(1, T+1):
+    ls = input()
+
+    # print(ls)
+    stack = []
+    answer = 1
+    for i in ls:
+        if i == "'" or i == '"':
+            pass
+            # 이부분 다시 해야함
+
+        # i가 (, {이면 stack에 append
+        if i in '({':
+            stack.append(i)
+        # i가 )이면 stack.pop으로 (가 나오는지 확인
+        elif i == ')':
+            if stack.pop() == '(':
+                continue
+            else:
+                answer = 0
+                break
+        # i가 }이면 stack.pop으로 {가 나오는지 확인
+        elif i == '}':
+            if stack.pop() == '{':
+                continue
+            else:
+                answer = 0
+                break
+        else:
+            continue
+    # stack의 길이가 0보다 크면 stack을 다 비우지 못했으므로 실패
+    if len(stack) > 0:
+        answer = 0
+
+    print(f'#{test_case} {answer}')
