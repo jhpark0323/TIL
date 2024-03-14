a219c082d5b5d25d11c43aedc152ac18cd854772
jhpark <brianwogus@naver.com>
2024-03-14 17:43:50 +0900

    django_0314

---
+++ 05_django/0314/03-django-urls/articles/urls.py
@@ -0,0 +1,30 @@
+"""
+URL configuration for firstpjt project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+# 명시적 상대경로
+from . import views
+
+app_name = 'articles'
+urlpatterns = [
+    path('index/', views.index, name='index'),
+    path('dinner/', views.dinner, name='dinner'),
+    path('search/', views.search, name='search'),
+    path('throw/', views.throw, name='throw'),
+    path('catch/', views.catch, name='catch'),
+    path('greeting/<str:name>/', views.greeting, name='greeting'),
+    path('articles/<int:num>/', views.detail, name='detail'),
+]
