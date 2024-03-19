8cb01162a336f0da76880ed4ee4e97ddf954298f
jhpark <brianwogus@naver.com>
2024-03-19 17:47:12 +0900

    algorithm_0319

--- 03_algorithm_II/05_백트래킹/1865_동철이의일분배/sol.py
+++ 03_algorithm_II/05_백트래킹/1865_동철이의일분배/sol.py
@@ -1,2 +1,34 @@
 import sys
 sys.stdin = open('input.txt')
+
+import decimal
+
+def back(level, val):
+    global max_val
+
+    if max_val >= val:
+        return
+
+    if level == n:
+        if max_val < val:
+            max_val = val
+        return
+
+    for j in range(level, n):
+        p[level], p[j] = p[j], p[level]
+        back(level+1, val*0.01*arr[level][p[level]])
+        p[level], p[j] = p[j], p[level]
+
+T = int(input())
+
+for test_case in range(1, T+1):
+    n = int(input())
+    arr = [list(map(int, input().split())) for _ in range(n)]
+    p = [i for i in range(n)]
+
+    max_val = -float('inf')
+    back(0, 1)
+
+    # answer = round(max_val*100, 7)
+    answer = round(decimal.Decimal(max_val*100), 6)
+    print(f'#{test_case} {answer}')
--- 03_algorithm_II/05_백트래킹/5208_전기버스2/sol.py
+++ 03_algorithm_II/05_백트래킹/5208_전기버스2/sol.py
@@ -1,2 +1,38 @@
 import sys
 sys.stdin = open('input.txt')
+
+def back(level, cnt, battery):
+    global min_cnt
+
+    if min_cnt <= cnt:
+        return
+
+    if level == n-1:
+        if battery <= 0:
+            return
+        else:
+            if min_cnt > cnt:
+                min_cnt = cnt
+            return
+
+    if level < n-1 and battery > 0:
+        # battery교체한 경우
+        back(level+1, cnt+1, ls[level])
+        # 교체 안한경우
+        back(level+1, cnt, battery-1)
+
+
+
+T = int(input())
+
+for test_case in range(1, T+1):
+    n, *ls = list(map(int, input().split()))
+    # print(n)
+    # print(ls)
+
+    # cnt_ls = []
+    min_cnt = float('inf')
+
+    back(1, 0, ls[0])
+    # print(cnt_ls)
+    print(f'#{test_case} {min_cnt}')
