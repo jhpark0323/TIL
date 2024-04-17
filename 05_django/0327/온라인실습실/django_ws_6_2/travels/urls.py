47ca547ec8f1e0f2b37a09fc388d9a11183ccbf4
jhpark <brianwogus@naver.com>
2024-04-17 16:51:57 +0900

    0417

--- 05_django/0327/온라인실습실/django_ws_6_2/travels/urls.py
+++ 05_django/0327/온라인실습실/django_ws_6_2/travels/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'travels'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create/', views.create, name='create'),
+    path('<int:pk>/', views.detail, name='detail'),
 ]
