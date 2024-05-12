d4a2ca54f22c7c62c9dd6c92a45899915ecf7697
jhpark0323 <brianwogus@naver.com>
2024-05-12 17:33:02 +0900

    0512

---
+++ 08_vue/0507-04-component-state-flow/온라인실습실/vue_ex_5_3/src/components/ProductList.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul>
+    <ProductListItem
+      @add-to-cart="addToCart(product)"
+      v-for="product in products" 
+      :key="product.id" 
+      :product="product"
+    />
+  </ul>
+</template>
+
+<script setup>
+import ProductListItem from '@/components/ProductListItem.vue'
+
+defineProps({
+  products: Array
+})
+
+const emit = defineEmits(['addToCart'])
+
+const addToCart = (product) => {
+  emit('addToCart', product.name, product.price)
+}
+
+</script>
